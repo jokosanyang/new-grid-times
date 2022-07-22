ee216988a40f06def87491d453bbe36d99573ad6
Joko Sanyang <joko.sanyang@ticketmaster.co.uk>
2022-07-22 12:45:49 +0100

    Task 1: Desktop header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,10 @@
-import React from 'react';
-import styled from 'styled-components/macro';
-import { Menu, Search, User } from 'react-feather';
-
-import { QUERIES } from '../../constants';
-
-import MaxWidthWrapper from '../MaxWidthWrapper';
-import Logo from '../Logo';
-import Button from '../Button';
+import React from "react";
+import styled from "styled-components/macro";
+import { Menu, Search, User } from "react-feather";
+import { QUERIES } from "../../constants";
+import MaxWidthWrapper from "../MaxWidthWrapper";
+import Logo from "../Logo";
+import Button from "../Button";
 
 const Header = () => {
   return (
@@ -31,6 +29,21 @@ const Header = () => {
       <MainHeader>
         <Logo />
       </MainHeader>
+      <DesktopHeader>
+        <ActionGroup>
+          <button>
+            <Search size={24} />
+          </button>
+          <button>
+            <Menu size={24} />
+          </button>
+        </ActionGroup>
+        <Logo />
+        <Subscribe>
+          <Button>Subscribe</Button>
+          <LogIn href="/">Already a subscriber?</LogIn>
+        </Subscribe>
+      </DesktopHeader>
     </header>
   );
 };
@@ -39,6 +52,10 @@ const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
   color: white;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: none;
+  }
 `;
 
 const Row = styled(MaxWidthWrapper)`
@@ -65,6 +82,34 @@ const MainHeader = styled(MaxWidthWrapper)`
   justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: none;
+  }
+`;
+
+const DesktopHeader = styled(MaxWidthWrapper)`
+  display: none;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-top: 32px;
+    margin-bottom: 48px;
+  }
+`;
+
+const Subscribe = styled(ActionGroup)`
+  flex-direction: column;
+  align-items: center;
+  gap: 4px;
+`;
+
+const LogIn = styled.a`
+  font-style: italic;
+  font-size: 14px;
+  text-decoration: underline;
 `;
 
 export default Header;
